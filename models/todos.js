32db24193fcc01f313b9bc4be12b7055a7166674
B Kleinen <kleinen@infrastructure.de>
2020-12-08 21:55:54 +0100

    list pomodori in todo

--- models/todos.js
+++ models/todos.js
@@ -3,6 +3,13 @@ const pomodoroSchema = mongoose.Schema({
     start: Date,
     end: Date
 })
+pomodoroSchema.methods.df = (date) =>{
+// https://stackoverflow.com/questions/10645994/how-to-format-a-utc-date-as-a-yyyy-mm-dd-hhmmss-string-using-nodejs
+    return date.toISOString().
+    replace(/T/, ' ').      // replace T with a space
+        replace(/\..+/, '')
+}
+
 const todoSchema = mongoose.Schema({
     title: String,
     done: Boolean,
