959606f3298d75c0835d1d404a0fcee73c3b9563
B Kleinen <kleinen@infrastructure.de>
2020-12-08 11:22:58 +0100

    add express-ejs-layouts template and use it in the views

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+var expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
